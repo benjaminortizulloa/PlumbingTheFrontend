756dc5525185a50e577da8f5321cdda227b4f46e
benjaminortizulloa <plzbeemyfriend@gmail.com>
2020-12-03 12:00:13 -0600

    allowlist cors

--- api/api.R
+++ api/api.R
@@ -1,8 +1,12 @@
 source('functions.R')
 
 #* @filter cors
-cors <- function(res) {
-  res$setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500")
+cors <- function(req, res) {
+  allowlist <- c("http://127.0.0.1:5500")
+
+  if(req$HTTP_ORIGIN %in% allowlist){
+    res$setHeader("Access-Control-Allow-Origin", req$HTTP_ORIGIN)
+  }
   res$setHeader('Vary', 'Origin')
   plumber::forward()
 }
