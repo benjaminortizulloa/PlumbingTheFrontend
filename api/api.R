d300ce7ceb090da50a90473590914303e1f832d8
benjaminortizulloa <plzbeemyfriend@gmail.com>
2020-12-03 04:15:53 -0600

    more secure cors

--- api/api.R
+++ api/api.R
@@ -2,7 +2,8 @@ source('functions.R')
 
 #* @filter cors
 cors <- function(res) {
-  res$setHeader("Access-Control-Allow-Origin", "*")
+  res$setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500")
+  res$setHeader('Vary', 'Origin')
   plumber::forward()
 }
 
