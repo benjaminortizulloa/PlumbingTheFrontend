e35157f31d480f6ceef83b9c74bcac5811b23b89
benjaminortizulloa <plzbeemyfriend@gmail.com>
2020-12-03 19:00:32 -0600

    actions needed

--- frontend/code/login.js
+++ frontend/code/login.js
@@ -1,10 +1,12 @@
 let suggestions = d3.select('header')
     .append('button')
-    .append('a')
     .attr('class', 'nav')
     .style("font-size", "3vh")
-    .attr('href', '/suggestions')
+    .style('display', 'none')
     .html('Suggestions')
+    .on('click', function () {
+        window.location.href = '/suggestions'
+    })
 
 let login = d3.select('header')
     .append('button')
@@ -16,6 +18,8 @@ if (document.cookie) {
         window.location.href = '/'
         document.cookie = "jwt=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=localhost;";
     }).html('Logout')
+
+    suggestions.style('display', 'inline')
 } else {
     login.on('click', function () {
         window.location.href = 'http://github.com/login/oauth/authorize?client_id=b823783bcb81ca97c4b9'
