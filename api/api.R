0c6a4df5202f6eeaaca35b5ce5463fa9dc2b0512
benjaminortizulloa <plzbeemyfriend@gmail.com>
2020-12-04 01:53:22 -0600

    last minute cleanup

--- api/api.R
+++ api/api.R
@@ -2,7 +2,7 @@ source('functions.R')
 
 #* @filter cors
 cors <- function(req, res) {
-  allowlist <- c("http://127.0.0.1:5500")
+  allowlist <- c("http://127.0.0.1:5500", "http://localhost:5500")
 
   if(req$HTTP_ORIGIN %in% allowlist){
     res$setHeader("Access-Control-Allow-Origin", req$HTTP_ORIGIN)
